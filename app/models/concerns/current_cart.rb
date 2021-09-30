ae1b9ec17012e8b99817fb764432ddb673185a2c
Wilsonk <wilson.kameni001@umb.edu>
2021-09-29 23:19:17 -0400

    removing current cart

--- app/models/concerns/current_cart.rb
+++ app/models/concerns/current_cart.rb
@@ -1,12 +1,12 @@
-# adding cookies in rails that youre browsing
-module CurrentCart
+# # adding cookies in rails that youre browsing
+# module CurrentCart
 
-    private
+#     private
 
-    def set_cart
-        @cart = Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-        @cart = Cart.create
-        session[:cart_id] = @cart_id
-    end
-end
+#     def set_cart
+#         @cart = Cart.find(session[:cart_id])
+#     rescue ActiveRecord::RecordNotFound
+#         @cart = Cart.create
+#         session[:cart_id] = @cart_id
+#     end
+# end
