304cb0c2ded8c3a27d376ac297af398ec5f815a8
Wilsonk <wilson.kameni001@umb.edu>
2021-09-29 23:13:56 -0400

    carts controller

--- app/models/concerns/current_cart.rb
+++ app/models/concerns/current_cart.rb
@@ -6,7 +6,7 @@ module CurrentCart
     def set_cart
         @cart = Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
-        @cart = Cart.create
+        @carts = Cart.create
         session[:cart_id] = @cart_id
     end
 end
